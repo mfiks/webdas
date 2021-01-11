cf552ca86a81cf286f51476b632e2dd3982c74ab
mfiks <fikri.setiawan182@gmail.com>
2021-01-11 12:55:51 +0700

    repominggu10

---
+++ latihanjavascript/eksternal-js/kode.js
@@ -0,0 +1,5 @@
+alert("Hello, ini adalah program JS eksternal!");
+var title = "Belajar Pemrograman Javascript";
+var site_Name = "Belajar Kode";
+var url = "http://dinus.ac.id";
+var visitorCount = 1000; 
